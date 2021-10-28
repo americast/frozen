2e27962f129f5f7bb3af27e285970893d965fb54
americast <sayan.sinha@iitkgp.ac.in>
2021-10-28 16:01:58 -0400

    Added tb

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import torch
 import data
 from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+import math
+
+writer = SummaryWriter("embedded_encoder")
+EPOCHS = 10000
 
 # Initialise the pretrained language model
 tokenizer          = T5Tokenizer.from_pretrained("t5-large")
@@ -31,32 +37,37 @@ dataloader = DataLoader(data_here, batch_size=4,
                         shuffle=True, num_workers=0)
 
 loss_fct = CrossEntropyLoss(ignore_index=-100)
+prev_loss = math.inf
 
-for img, label in dataloader:
-    # Pass through vision module
-    out_vis = model_vis(img)
-    out_vis_ext = model_vis_ext(out_vis)
-    out_vis_ext_1 = out_vis_ext[:, :1024].unsqueeze(1)
-    out_vis_ext_2 = out_vis_ext[:, 1024:].unsqueeze(1)
+for e in range(EPOCHS):
+    losses = []
+    for img, label in tqdm(dataloader):
+        # Pass through vision module
+        out_vis = model_vis(img)
+        out_vis_ext = model_vis_ext(out_vis)
+        out_vis_ext_1 = out_vis_ext[:, :1024].unsqueeze(1)
+        out_vis_ext_2 = out_vis_ext[:, 1024:].unsqueeze(1)
 
-    max_source_length = max_target_length = 1000
+        max_source_length = max_target_length = 1000
 
-    # Pass through the embedder
-    input_sequences = list(label)
-    encoding = tokenizer([sequence for sequence in input_sequences],
-                        padding='longest',
-                        max_length=max_source_length,
-                        truncation=True,
-                        return_tensors="pt")
-    out_embedder = model_lang_embed(encoding["input_ids"])
-    lang_input = torch.cat([out_vis_ext_1, out_vis_ext_2, out_embedder], axis=1)
-    decoder_output = model_lang(inputs_embeds=lang_input, labels=encoding["input_ids"])
-    loss = decoder_output.loss
-    loss.backward()
+        # Pass through the embedder
+        input_sequences = list(label)
+        encoding = tokenizer([sequence for sequence in input_sequences],
+                            padding='longest',
+                            max_length=max_source_length,
+                            truncation=True,
+                            return_tensors="pt")
+        out_embedder = model_lang_embed(encoding["input_ids"])
+        lang_input = torch.cat([out_vis_ext_1, out_vis_ext_2, out_embedder], axis=1)
+        decoder_output = model_lang(inputs_embeds=lang_input, labels=encoding["input_ids"])
+        loss = decoder_output.loss
+        loss.backward()
+        losses.append(loss)
 
-# initialise CC dataset
+    loss_here = sum(losses)/len(losses)
+    print("Epoch: "+str(e)+"; loss: "+str(loss_here))
+    if loss_here < prev_loss:
+        prev_loss = loss_here
+        torch.save(model_vis.state_dict(), "saved_models/model_vis_"+str(e)+".pth")
 
-# check how to use t5
-# start training after setting zero_grad
-
-input_ids = tokenizer('The <extra_id_0> walks in <extra_id_1> park', return_tensors='pt').input_ids
+    writer.add_scalar('Loss/train', sum(losses)/len(losses), e)
