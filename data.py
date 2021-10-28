1d52e95bc4c7dfa0f87ef68e22cd147d05adc28a
americast <sayan.sinha@iitkgp.ac.in>
2021-10-28 18:54:22 -0400

    start training

--- data.py
+++ data.py
@@ -4,22 +4,21 @@ import pudb
 from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 import csv
 from tqdm import tqdm
 import zlib
 import os
 import torch.nn.functional as F
+import pickle
 
 class CCD(Dataset):
     def __init__(self, transform=None, target_transform=None):
-        self.tsv_file_loc = "/coc/dataset/conceptual_caption/DownloadConceptualCaptions/Train_GCC-training.tsv"
         self.tsv_folder = "/coc/dataset/conceptual_caption/DownloadConceptualCaptions/training/"
-        f = open(self.tsv_file_loc, "r")
-        read_tsv = csv.reader(f, delimiter="\t")
-        self.tsv_list = []
-        for row in tqdm(read_tsv):
-            self.tsv_list.append(row)
+        f = open("tsv_list_training.pkl", "rb")
+        self.tsv_list = pickle.load(f)
+        print(len(self.tsv_list))
+        f.close()
         self.transform = transform
         self.target_transform = target_transform
 
@@ -28,13 +27,19 @@ class CCD(Dataset):
 
     def __getitem__(self, idx):
         folder = "training"
-        url     = self.tsv_list[idx][1]
-        name = zlib.crc32(url.encode('utf-8')) & 0xffffffff
+        url, name     = self.tsv_list[idx][1], self.tsv_list[idx][2]
         img_path = os.path.join(self.tsv_folder, str(name))
-        image = read_image(img_path) /255
+        try:
+            image = read_image(img_path, mode=ImageReadMode.RGB) /255
+        except:
+            url, name     = self.tsv_list[0][1], self.tsv_list[0][2]
+            img_path = os.path.join(self.tsv_folder, str(name))
+            image = read_image(img_path, mode=ImageReadMode.RGB) /255
+
         image = F.interpolate(image.unsqueeze(0), size=(512,512)).squeeze(0)
         label = self.tsv_list[idx][0]
         if self.transform:
             image = self.transform(image)
+        
         return image, label
 
