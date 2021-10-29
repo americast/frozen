c8bd75d4efabe0459077232c8f147965bc6f95fa
americast <sayan.sinha@iitkgp.ac.in>
2021-10-28 20:26:57 -0400

    add data prep

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.nn import CrossEntropyLoss
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import math
+import os
 
 writer = SummaryWriter("embedded_encoder")
 EPOCHS = 10000
@@ -33,16 +34,32 @@ model_vis_ext = torch.nn.Sequential(torch.nn.Linear(1000,2048))
 data_here = data.CCD(transform = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225]))
 
-dataloader = DataLoader(data_here, batch_size=4,
-                        shuffle=True, num_workers=0)
+dataloader = DataLoader(data_here, batch_size=160,
+                        shuffle=True, num_workers=2)
 
 loss_fct = CrossEntropyLoss(ignore_index=-100)
 prev_loss = math.inf
+
+num_gpus = os.environ['CUDA_VISIBLE_DEVICES'].split(',').__len__()
+os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(f'{i}' for i in range(num_gpus))
+
+model_vis = model_vis.cuda()
+model_vis = torch.nn.DataParallel(model_vis)
+
+model_vis_ext = model_vis_ext.cuda()
+model_vis_ext = torch.nn.DataParallel(model_vis_ext)
+
+model_lang_embed = model_lang_embed.cuda()
+model_lang_embed = torch.nn.DataParallel(model_lang_embed)
+
+model_lang = model_lang.cuda()
+model_lang = torch.nn.DataParallel(model_lang)
 
 for e in range(EPOCHS):
     losses = []
     for img, label in tqdm(dataloader):
         # Pass through vision module
+        img = img.cuda()
         out_vis = model_vis(img)
         out_vis_ext = model_vis_ext(out_vis)
         out_vis_ext_1 = out_vis_ext[:, :1024].unsqueeze(1)
@@ -57,11 +74,12 @@ for e in range(EPOCHS):
                             max_length=max_source_length,
                             truncation=True,
                             return_tensors="pt")
+        encoding["input_ids"] = encoding["input_ids"].cuda()
         out_embedder = model_lang_embed(encoding["input_ids"])
         lang_input = torch.cat([out_vis_ext_1, out_vis_ext_2, out_embedder], axis=1)
         decoder_output = model_lang(inputs_embeds=lang_input, labels=encoding["input_ids"])
         loss = decoder_output.loss
-        loss.backward()
+        loss.sum().backward()
         losses.append(loss)
 
     loss_here = sum(losses)/len(losses)
---
+++ prep_data.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+import pickle
+import csv
+import zlib
+import os
+
+tsv_file_loc = "/coc/dataset/conceptual_caption/DownloadConceptualCaptions/Train_GCC-training.tsv"
+tsv_folder = "/coc/dataset/conceptual_caption/DownloadConceptualCaptions/training/"
+f = open(tsv_file_loc, "r")
+read_tsv = csv.reader(f, delimiter="\t")
+tsv_list = []
+for row in tqdm(read_tsv):
+    url = row[1]
+    name = zlib.crc32(url.encode('utf-8')) & 0xffffffff
+    img_path = os.path.join(tsv_folder, str(name))
+    if os.path.isfile(img_path):
+        tsv_list.append([row[0], row[1], name])
+
+with open('tsv_list_training.pkl', 'wb') as f:
+   pickle.dump(tsv_list, f)
